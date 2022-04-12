f3d44279bf0c7e2c233596dd3837011706045a04
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2022-04-12 07:45:11 +0700

    menyesuaikan fungsi myth auth pada laman login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -15,30 +15,54 @@
                         <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4"><?=lang('Auth.loginTitle')?></h1>
                                 </div>
-                                <form class="user">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">Remember
-                                                Me</label>
+                                
+                                <?= view('Myth\Auth\Views\_message_block') ?>
+
+                                <form class="user" action="<?= route_to('login') ?>" method="post">
+                                    <?= csrf_field() ?>
+
+                                    <?php if ($config->validFields === ['nis']): ?>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user <?php if(session('errors.login')) : ?>is-invalid<?php endif ?>" 
+                                                name="nis" placeholder="NIS">
+                                            <div class="invalid-feedback">
+								                <?= session('errors.login') ?>
+							                </div>
                                         </div>
+                                    <?php endif; ?>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user <?php if(session('errors.password')) : ?>is-invalid<?php endif ?>" 
+                                               name="password" placeholder="<?=lang('Auth.password')?>">
+                                        <div class="invalid-feedback">
+								            <?= session('errors.password') ?>
+							            </div>
                                     </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
+                                    
+                                    <?php if ($config->allowRemembering): ?>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" name="remember" <?php if(old('remember')) : ?> checked <?php endif ?>>
+                                                <label class="custom-control-label" for="customCheck">
+                                                    <?=lang('Auth.rememberMe')?>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    <?php endif; ?>
+
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        <?=lang('Auth.loginAction')?>
+                                    </button>
                                 </form>
+
                                 <hr>
-                                <div class="text-center">
-                                    <a class="small" href="register.html">Create an Account!</a>
-                                </div>
+                                <?php if ($config->allowRegistration) : ?>
+                                    <div class="text-center">
+                                        <p><a class="small" href=<?= route_to('register') ?>><?=lang('Auth.needAnAccount')?></a></p>
+                                    </div>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
