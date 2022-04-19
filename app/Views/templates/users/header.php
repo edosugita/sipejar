65a266a2932731dac073fc6a0dc13e0f9090c0fd
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2022-04-19 10:04:59 +0700

    edit gitignore dan header

--- app/Views/templates/users/header.php
+++ app/Views/templates/users/header.php
@@ -8,7 +8,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a href="" class="nav-link nav-name disabled">Edo Sugita Ibrahim</a>
+                        <a href="" class="nav-link nav-name disabled"><?php user() ?></a>
                     </li>
                     <li class="nav-item dropdown">
                         <div class="img-profile" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -16,7 +16,7 @@
                         </div>
 
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                            <li><a class="dropdown-item" href="<?= base_url('logout') ?>">Logout</a></li>
                         </ul>
                     </li>
                 </ul>
